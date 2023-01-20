a58146e5a8a0fe161a81442def536f728d2c22b8
Mikey Ferguson <mikeyferg49@gmail.com>
2023-01-20 16:44:46 -0600

    unbounded knapsack complete

--- Java/Solution.java
+++ Java/Solution.java
@@ -1,24 +1,94 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        // Coin change problem
-        ArrayList<Long> coins = new ArrayList<>();
-        coins.add((long)2);
-        coins.add((long)5);
-        coins.add((long)3);
-        coins.add((long)6);
-        System.out.println(getWays(10, coins));
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        // longest common "substring" problem
-        System.out.println(substringDiff(2, "tabriz", "torino"));
+    public static void main(String[] args) {
+        try {
+
+            // String[] nm = reader.readLine()
+            //     .trim()
+            //     .split(" ");
+            // int n = Integer.parseInt(nm[0]);
+            // int m = Integer.parseInt(nm[1]);
+            // int[] coins = Arrays.stream(
+            //     reader.readLine()
+            //         .trim()
+            //         .split(" ")
+            //     )
+            //     .mapToInt(Integer::parseInt)
+            //     .toArray();
+
+            // System.out.printf(
+            //     "%d\n",
+            //     Solution.solve(n, m, coins)
+            // );
+
+            // // Coin change problem
+            // ArrayList<Long> coinsList = new ArrayList<>();
+            // coinsList.add((long)2);
+            // coinsList.add((long)5);
+            // coinsList.add((long)3);
+            // coinsList.add((long)6);
+            // System.out.println(getWays(10, coinsList));
+
+            // // longest common "substring" problem
+            // System.out.println(substringDiff(2, "tabriz", "torino"));
+
+            // unbounded knapsack
+            ArrayList<Integer> weights = new ArrayList<>();
+            weights.add(3);
+            weights.add(4);
+            weights.add(4);
+            weights.add(4);
+            weights.add(8);
+            System.out.println(unboundedKnapsack(9, weights));
+
+        } catch (Exception e){
+            System.out.println("Hello?");
+        }
     }
 
     //////////////////////////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * This is Dr. Ian Riley (from Tulsa University)'s method for this problem
+     * @param n
+     * @param m
+     * @param coins
+     * @return
+     */
+    public static long solve(int n, int m, int[] coins) {
+        Arrays.sort(coins);
+        long[][] cache = new long[n+1][m+1];
+        for (int j=0; j<m+1; j++){
+            cache[0][j] = 0;
+        }
+        for (int i=0; i<n+1; i++){
+            cache[i][0] = 0;
+        }
+        for (int i=1; i<=n; i++){
+            for (int j=2; j<=m; j++){
+                if (coins[j-1]>i) {
+                    // can't include this coin
+                    cache[i][j] = cache[i][j-1];
+                } else if (coins[j-1] == i) {
+                    cache[i][j] = 1 + cache[i][j-1];
+                } else {
+                    // number of ways to include this coin PLUS the number of ways to NOT include this coin
+                    cache[i][j] = 
+                    cache[i - coins[j-1]][j] // coins has not been formatted correctly
+                    // coins[j-1] is SMALLER than i because we are in this section of the conditional
+                        + cache[i][j-1];
+                }
+            }
+        }
+
+        return cache[n][m];
+    }
 
     /**
      * Given an amount and the denominations of coins available, determine how many ways change can be made for amount. There is a limitless supply of each coin type.
@@ -107,5 +177,62 @@ public class Solution {
     public static int substringDiff(int k, String s1, String s2) {
         return 0;
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /*
+     * Complete the 'unboundedKnapsack' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     * 1. INTEGER k
+     * 2. INTEGER_ARRAY arr
+     */
+
+    public static int unboundedKnapsack(int k, List<Integer> arr) {
+        // first convert to an array and sort it
+        int[] objectWeights = arr.stream()
+                        .mapToInt(num -> num)
+                        .toArray();
+        Arrays.sort(objectWeights);
+
+        // Write your code here
+        int[][] table = new int[k+1][objectWeights.length];
+        // top row is capacity 0 - leave it alone
+
+        // base cases for top row
+        if (objectWeights[0] == 1){
+            // we have 1's across out top row
+            for (int i=0; i<objectWeights.length; i++){
+                table[1][i] = 1;
+            }
+        }
+
+        // base cases for left-most column
+        for (int r=1; r<=k; r++){
+            table[r][0] = r - (r % objectWeights[0]);
+        }
+
+        // now for the dynamic programming aspect
+        for (int capacity=2; capacity<=k; capacity++){
+            for (int j=1; j<objectWeights.length; j++){
+                if (objectWeights[j] > capacity){
+                    // we cannot use this object - it is too heavy so take the best solution NOT using this coin
+                    table[capacity][j] = table[capacity][j-1];
+                } else {
+                    // we CAN use this object - but should we? 
+                    // we know whether we do or don't, our ultimate set of items WILL fit in the bag, so take the max of the values yielded by the two choices
+                    table[capacity][j] = Math.max(table[capacity][j-1], objectWeights[j] + table[capacity-objectWeights[j]][j]);
+                }
+            }
+        }
+
+        // return target cell
+        return table[k][objectWeights.length-1];
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
