bb59a282b3ea4f8afbfcb3c92a80d939bf93f148
Mikey Ferguson <mikeyferg49@gmail.com>
2023-02-17 17:53:14 -0600

    redid C++ organization

---
+++ C++/SwapPairs/src/Solution1.h
@@ -0,0 +1,18 @@
+#ifndef Solution1_H
+#define Solution1_H
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution1 {
+
+};
+
+#endif
