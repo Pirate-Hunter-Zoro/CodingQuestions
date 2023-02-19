eac57d812b7279e184a9dd8a8dcccbda7afd5c40
Mikey Ferguson <mikeyferg49@gmail.com>
2023-02-19 13:49:16 -0600

    added rough draft code for reversing k-group nodes

--- C++/src/include/Solution1.h
+++ C++/src/include/Solution1.h
@@ -2,8 +2,53 @@
 #define Solution1_H
 
 #include "ListNode.h"
+#include <stack>
 
 class Solution1 {
+
+private:
+    /**
+     * @brief Reverse the order of the next k nodes in the linked list segment
+     * 
+     * @param current 
+     * @param k 
+     * @return ListNode* 
+     */
+    ListNode* invert(ListNode* current, int k){
+        // we need to create a stack of nodes, and unpop the stack to reverse the order
+        std::stack<ListNode*> reversed_order;
+        ListNode* toReturn;
+        ListNode* toFollow;
+        ListNode* currentNode = current;
+        for (int i=0; i<k; i++){
+            if (i < k-1){
+                // not the last one
+                reversed_order.push(current);
+                currentNode = currentNode->next;
+            } else{
+                // last one
+                toReturn = currentNode;
+                toFollow = currentNode->next;
+            }
+        }
+
+        ListNode* previous = toReturn;
+        ListNode* nextNode;
+        // now unpop the stack
+        while (!reversed_order.empty()){
+            nextNode = reversed_order.top(); // peek
+            previous->next = nextNode;
+            reversed_order.pop(); // pop
+            previous = nextNode;
+
+            if (reversed_order.empty()){
+                // last one
+                nextNode->next = toFollow;
+            }
+        }
+
+        return toReturn;
+    }
 
 public:
     ListNode *swapPairs(ListNode *head)
@@ -32,6 +77,40 @@ public:
         }
 
         return toReturn;
+    }
+
+    /**
+     * @brief Reverse every k group of nodes - if the lenght of the linked list is not divisible by k leave the last group of nodes untouched
+     * 
+     * @param head 
+     * @param k 
+     * @return ListNode* 
+     */
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // first find the length of the linked list
+        ListNode* current = head;
+        int length = 0;
+        while (current != nullptr){
+            length++;
+            current = current->next;
+        }
+
+        // now that we have the length
+        ListNode* toReturn = head;
+        for (int i=0; i<length / k; i++){
+            if (i==0) {
+                // keep track of the head to return
+                head = this->invert(current, k);
+            } else {
+                // just invert
+                this->invert(current, k);
+            }
+            current = current->next;
+        }
+
+        // now that the inversion is done
+        return toReturn;
+
     }
 };
 
--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 int main() {
     
+    ListNode* head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    Solution1 sol = Solution1();
+    sol.reverseKGroup(head, 2);
 
     return 0;
 }
