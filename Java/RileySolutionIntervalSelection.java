3594b78e7340691c2a90167d6897b94b227046c4
Mikey Ferguson <mikeyferg49@gmail.com>
2023-03-03 19:45:06 -0600

    removed redundant Java classes

--- Java/RileySolutionIntervalSelection.java
+++ Java/RileySolutionIntervalSelection.java
@@ -1,3 +1,12 @@
+import java.io.*;
+import java.util.*;
+
 public class RileySolutionIntervalSelection {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) {
+        
+    }
     
 }
