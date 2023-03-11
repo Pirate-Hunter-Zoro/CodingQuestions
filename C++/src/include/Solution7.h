212083fbfbab09686d17a5f88db64560dd6944be
Mikey Ferguson <mikeyferg49@gmail.com>
2023-03-11 16:36:33 -0600

    added skeleton for shortest palindrome

---
+++ C++/src/include/Solution7.h
@@ -0,0 +1,23 @@
+#ifndef Solution7_H
+#define Solution7_H
+
+#include <string>
+
+class Solution7{
+
+public:
+    /**
+     * @brief You are given a string s. 
+     * You can convert s to a palindrome by adding characters in front of it.
+     * Return the shortest palindrome you can find by performing this transformation.
+     *
+     * @param s
+     * @return std::string
+     */
+    std::string shortestPalindrome(std::string s)
+    {
+        
+    }
+};
+
+#endif
